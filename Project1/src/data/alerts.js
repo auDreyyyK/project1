41336f6fbfd21d37c6f21be2f0955608ddedab56
auDreyyyK <audreytazo@gmail.com>
2025-04-02 18:14:03 -0400

    ajout des donnée fictive

---
+++ Project1/src/data/alerts.js
@@ -0,0 +1,16 @@
+const alerts = [
+    {
+        id:1,
+        title: "Fermeture à la circulation sur la rue Bélanger, entre la 8e avenue et le boulevard Saint-Michel, Villeray-Saint-Michel-Parc-Extension",
+        date: "2 avril",
+        detail: "À ‌partir‌ ‌du‌ 03/04/2025 : • Fermeture complète de : - Rue Bélanger direction est, entre la 8e Avenue et le boulevard Saint-Michel • Fermeture partielle de : - Rue Bélanger dans les deux directions, entre la 8e Avenue et la rue Molson.Informations • Raison‌ ‌:‌ ‌travaux‌ ‌de‌ ‌réhabilitation de conduite d'égout; • Horaire : 24h/24; • La réalisation de ces travaux est estimée à une semaine; • Retour à la normale prévu le 10/04/2025; • Détours prévus; • Stationnement non disponible durant cette période à proximité de la zone en travaux.‌  "
+    },
+
+    {
+        id:2,
+        title: "MISE À JOUR Collecte reportée des déchets domestiques du 25/03/2025 – Arrondissement d'Ahuntsic Cartierville",
+        date: "26 mars",
+        detail: "• Collecte des déchets domestiques du 25/03/205 est reportée au 26/03/2025 - Entre les rues Cousineau et Camille, il reste environ 1/3 de la superficie à couvrir. • Raison : bris d'équipement • Laissez les matières jusqu'au 26/03/2025."
+    }
+];
+export default alerts;
