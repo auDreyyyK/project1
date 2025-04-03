7679c4606a3f56743ce0f1e308a36d028bf7305a
auDreyyyK <audreytazo@gmail.com>
2025-04-03 17:18:16 -0400

    modification de Header, code de base dans SearchBar + css

--- Project1/src/components/Header.jsx
+++ Project1/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logoMontreal.svg";
 import "./Header.css";
-import { FaUser, FaBars, FaSearch } from "react-icons/fa";
+import { FaUser} from "react-icons/fa";
 
 
 export function Header() { 
@@ -13,23 +13,6 @@ export function Header() {
             <Link to="/">
                 <img src={logo} alt="Logo de la ville de mtl" className="logo-img" />
             </Link>
-           </div>
-
-           {/* -------------MENU------------*/}
-           <nav className="menu">
-            <button className="menu-button">
-                <FaBars className="menu-icon" />
-                <span>Menu</span>
-            </button>
-           </nav>
-
-           {/* -------------RECHERCHE------------*/}
-           <div className="search">
-            <button className="search-button">
-                <FaSearch className="search-icon" />
-                Recherche
-            </button>
-            
            </div>
 
            {/* -------------MON COMPTE------------*/}
--- Project1/src/components/SearchBar.jsx
+++ Project1/src/components/SearchBar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import "./SearchBar.css";
+import { FaChevronDown} from "react-icons/fa";
 
 const SearchBar = () => {
     return(
         <div>
-            <input type="text" placeholder="Que cherchez-vous?" />
+            <div className="searchSection">
+            <h2>Avis et Alertes</h2>
+            <p>Trouver un avis</p>
+            <input type="text" placeholder="Que cherchez-vous?" className="searchBar" />
             <button>Rechercher</button>
+            </div>
+            <div className="filter">
+                <button>Arrondissement <FaChevronDown className="chevron"/></button>
+                <button>Date <FaChevronDown className="chevron"/></button>
+                <button>Sujet <FaChevronDown className="chevron"/></button>
+            </div>
         </div>
     );
 };
