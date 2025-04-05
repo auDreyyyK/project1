89be796b0a5664f294ff2553986970721b168ad8
auDreyK <audreytazo@gmail.com>
2025-04-05 16:36:31 -0400

    message btn m'abonner

--- Project1/src/components/Subscribe.jsx
+++ Project1/src/components/Subscribe.jsx
@@ -1,5 +1,7 @@
 import { FaArrowRight } from "react-icons/fa";
 import React from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 
 const Subscribe = () =>{
@@ -7,7 +9,12 @@ const Subscribe = () =>{
         <div className="abonner">
             <h3>S'abonner aux alertes</h3>
             <p>Pour recevoir des avis et alertes par courriel ou texto, vous devez avoir créé un compte.</p>
-            <a href="#">M'abonner <FaArrowRight className="arrow"/></a>
+            <Popup 
+                trigger={<button className="subscribe-btn">
+                    M'abonner <FaArrowRight className="arrow"/></button>}
+                >
+                <p>Cette fonctionnalité n'est pas encore disponible</p> 
+            </Popup>
         </div>
     )
 }
